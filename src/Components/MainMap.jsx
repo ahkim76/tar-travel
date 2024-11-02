315e9f271681397c8e71678484236d4a3243a278
ahkim76 <ahkim9976@gmail.com>
2024-11-02 16:02:10 -0400

    Map styles

--- src/Components/MainMap.jsx
+++ src/Components/MainMap.jsx
@@ -23,9 +23,9 @@ function MainMap() {
             zoom={13} 
             scrollWheelZoom={true}>
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+  url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+  attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+/>
             <Marker position={[51.505, -0.09]}>
                 <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
