b28ff9b6e78240d00a309c9bdc00bcc4ce8be01f
wang24v <wang24v@ncssm.edu>
2024-11-02 17:01:07 -0400

    Small bug fix

--- webscraper.py
+++ webscraper.py
@@ -49,6 +49,7 @@ def create_txt_files():
                         continue
                     else:
                         info.append(t)
+                        found = True
         f.close()
         unis.append(University(id, name, info[1], info[0]))
         driver.quit()
