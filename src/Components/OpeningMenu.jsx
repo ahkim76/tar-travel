73c957dfeee105adc1f6886e4dbb4a8f8b20b954
ahkim76 <ahkim9976@gmail.com>
2024-11-03 00:52:42 -0400

    Almost finish w/ front end

--- src/Components/OpeningMenu.jsx
+++ src/Components/OpeningMenu.jsx
@@ -1,11 +1,20 @@
 import '../index.css'
+import { BsArrowRight } from "react-icons/bs";
 function OpeningMenu({ closeMenu }) {
     return (
-        <div className="opening-menu">
-            <h1 className="opening-header">Welcome to Tar Travel!</h1>
-            <p className="opening-paragraph">Use our interactive map to plan your study abroad travels</p>
-            <a href="#" onClick={closeMenu} className="opening-button" data-text="Click here to Enter!">Click here to Enter!</a>
-        </div>  
+        <>
+            <div className="opening-menu">
+                <h1 className="opening-header">Welcome to Tar Travel!</h1>
+                <p className="opening-paragraph">Use our interactive map to plan your study abroad travels</p>
+                <div className="button-container">
+                    <BsArrowRight className="arrow-icon"/>
+                    <a href="#" onClick={closeMenu} className="opening-button" data-text="Click on me to Enter!">Click on me to Enter!</a>
+                </div>
+                
+            </div>  
+            
+        </>
+        
     )
 }
 
