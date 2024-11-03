10288c917e99eb1e1495845bb500162edc6fff6a
Justin <thisisjustinmao@gmail.com>
2024-11-03 01:51:32 -0400

    Create summerjson.txt

--- webscraper.py
+++ webscraper.py
@@ -51,7 +51,7 @@ def get_university_objects(ids):
         url = "https://heelsabroad.unc.edu/_portal/tds-program-brochure?programid=" + str(i)
         driver.get(url)
 
-        time.sleep(2)
+        time.sleep(10)
 
         try:
             soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -134,8 +134,8 @@ def get_university_objects(ids):
                 elif experience_found and text != "":
                     student_experience = text
                     experience_found = False
-                
-        if overview == "" or location == None or student_experience == "" or coordinates == [0, 0]:
+        
+        if overview == "" or location == None or coordinates == [0, 0]:
             update_bad_file("From last check", i)
 
             continue
@@ -214,7 +214,7 @@ def getCoords(city, country, i):
 
 
 def update_bad_file(reason, i):
-    badfile = open("summerids.txt", "a")
+    badfile = open("badsummerids.txt", "a")
     badfile.write(reason + str(i))
     badfile.write("\n")
     badfile.close()
@@ -227,7 +227,6 @@ summer_ids = [12535, 12537, 12445, 12450, 12412, 12329, 12336, 12310, 12129, 120
 direct_enroll_ids = [12534, 10353, 10316, 10303, 10310, 10312, 10255, 10244, 10230, 10215, 10185, 10172, 10165, 10326, 10329, 10319, 12447, 10234, 12403, 10333, 10339, 10339, 10308, 10253, 10242, 10232, 10237, 10226, 10228, 10213, 10204, 10205, 10206, 10175, 10166, 10166, 10337, 11751, 10327, 10164, 10309, 10336, 10301, 10345, 10227, 10317, 12026, 10346, 10341, 10332, 10313, 10315, 10318, 10300, 10233, 10239, 10240, 10241, 10224, 10181, 11807, 10321, 10275, 10338, 12542, 10207]
 interships_ids = [12445, 12352, 10016, 10343, 10328, 10316, 10303, 10255, 10223, 10215, 10003, 10193, 10189, 10187, 10522, 10220, 12447, 12415, 11805, 12040, 10284, 10283, 10300, 12023, 12121, 12410, 10035, 12116, 10317, 12170, 10186, 10285, 10339, 10218, 10188, 10192, 10191, 10190, 12449, 10338, 10239]
 # cheap_ids = [11863, 10323, 12449, 10191, 10318, 10342, 10335, 10683, 12394, 10345, 10331, 10236, 10164, 11751, 10337, 10169, 10232, 11742, 10256, 10185, 10303]
-
 # exchange_ids = [10245, 10245]
 # get_university_objects(exchange_ids)
 get_university_objects(summer_ids)
